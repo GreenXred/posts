7bca0e8bb630891931f20a4563f4e515c3107fd1
GreenXred <antonzo09@mail.ru>
2025-10-19 22:42:28 +0300

    package

---
+++ my-app/src/components/root/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Root = () => <>
+    <div>Страница постов</div>
+    <Outlet />
+</>
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    loader: loadRootData,
+    children: [
+      {
+        path: "posts",
+        element: <Posts />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
